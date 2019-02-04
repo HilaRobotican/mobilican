6657a0d81e0b85d0bf7a24eccf491bbf3eff617e
Hila <hila@robotican.co>
2019-02-04 13:04:11 +0200

    image_snapshot_node is now allows capturing 3 images from each of the cameras.

--- mobilican_demos/image_snapshot/scripts/image_snapshot_node.py
+++ mobilican_demos/image_snapshot/scripts/image_snapshot_node.py
@@ -11,15 +11,18 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from image_snapshot.srv import *
 
-
 #usage for trx:
 # roslaunch mobilican trx.launch gazebo:=true cams:=true world:="/home/hila/catkin_ws/src/mobilican/mobilican_gazebo/worlds/simplified_market.world"
 # rosrun rqt_image_view rqt_image_view
 # rosrun rqt_robot_steering rqt_robot_steering
 # rosrun image_snapshot image_snapshot_node.py
 # rosservice call /image_snapshot_server // call the server from the command line
+
 IMAGES_DIRECTORY = '/home/hila/catkin_ws/src/mobilican/mobilican_demos/image_snapshot/images/'
-camera_dict = {0: "bottom", 1: "middle", 2: "top"}
+IMAGE_FORMAT = '.png'
+NUMBER_OF_CAMERAS = 3
+cameras = ["bottom", "middle", "top"]
+topics = ["/bottom_cam/image_raw", "/middle_cam/image_raw", "/top_cam/image_raw"]
 
 class image_snapshot_node:
   # A node that uses CvBridge to convert ROS images (sensor_msgs/Image) into OpenCV cv::Mat format.
@@ -27,8 +30,12 @@ class image_snapshot_node:
   # and holds the current frame from each camera. Upon a client request, it saves the images into files.
 
   def __init__(self):
-    self.counter = 0
-    self.current_images = [None] * 3  # the current image from each camera
+    self.counter = 0  # used for numbering the saved images.
+    self.current_images = [None] * NUMBER_OF_CAMERAS  # the current image from each camera
+    self.last_topic_time = [None] * NUMBER_OF_CAMERAS  # the last time each camera topic was called
+    self.camera_subscribers_ = [None] * NUMBER_OF_CAMERAS  # the subscribers to each camera topic
+    self.first_time_timer = True  # True if it is the first time that the timer is called
+
     self.bridge = CvBridge()
     self.init_camera_listeners()
     self.init_snapshot_server()
@@ -36,9 +43,10 @@ class image_snapshot_node:
   def init_camera_listeners(self):
     # Initialize listeners for each camera topic.
     # When new messages are received, callback functions are invoked with the message as the first argument.
-    self.bottom_cam_sub_ = rospy.Subscriber("/bottom_cam/image_raw", Image, self.update_image_callback, 0)
-    self.middle_cam_sub_ = rospy.Subscriber("/middle_cam/image_raw", Image, self.update_image_callback, 1)
-    self.top_cam_sub_ = rospy.Subscriber("/top_cam/image_raw", Image, self.update_image_callback, 2)
+    for i, topic in enumerate(topics):
+      self.camera_subscribers_[i] = rospy.Subscriber(topic, Image, self.update_image_callback, i)
+      self.last_topic_time[i] = False
+    rospy.Timer(rospy.Duration(1), self.on_timer)  # set on the timer.
 
   def init_snapshot_server(self):
     # Initialize a server named "image_snapshot_server", that receives a snapshot request from the client.
@@ -46,7 +54,23 @@ class image_snapshot_node:
     self.snapshot_server = rospy.Service("image_snapshot_server", ImageSnapshot, self.handle_image_snapshot)
     print("Ready to save images.")
 
+  def on_timer(self, event):
+    # Validate that each one of the camera's topics is updating each second.
+    if self.first_time_timer:
+      self.first_time_timer = False
+      return
+    # print('Timer called at ' + str(event.current_real))
+
+    for i, topic in enumerate(topics):
+      if self.last_topic_time[i] is False:
+        print("Error: one of the camera's topics is not updated.")
+      else:
+        self.last_topic_time[i] = False
+
   def update_image_callback(self, data, camera_index):
+    self.last_topic_time[camera_index] = True
+    # print("first image callback")
+
     try:
       # Converting an image message pointer to an OpenCV message:
       self.current_images[camera_index] = self.bridge.imgmsg_to_cv2(data, "bgr8")
@@ -60,17 +84,22 @@ class image_snapshot_node:
     # cv2.waitKey(3)
 
   def handle_image_snapshot(self, req):
-    # Save the current image into a file.
+    # Save the current images into files.
     print("server called")
-    for image, i in enumerate(self.current_images):
-      status = cv2.imwrite(IMAGES_DIRECTORY + camera_dict[i] + '_image_' + str(self.counter) + '.png', image)
-      print(camera_dict[i] + " image written to file-system : ", status)
+
+    for i, image in enumerate(self.current_images):
+      if image is None:
+        print("Error: no image exists.")
+      else:
+        status = cv2.imwrite(IMAGES_DIRECTORY + cameras[i] + '_image_' + str(self.counter) + IMAGE_FORMAT, image)
+        print(cameras[i] + " image written to file-system : ", status)
+
     self.counter += 1
     return "succeed"
 
 def main(args):
-  ic = image_snapshot_node()
   rospy.init_node('image_snapshot_node', anonymous=True)
+  im_snapshot = image_snapshot_node()
   try:
     rospy.spin()
   except KeyboardInterrupt:
